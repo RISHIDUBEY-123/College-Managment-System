e3cd8d6d0470033b60154a07079a7a03dcf86798
RISHI DUBEY <32414842+RISHIDUBEY-123@users.noreply.github.com>
2021-02-26 12:35:08 +0530

    Add files via upload

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -0,0 +1,48 @@
+package com.in28minutes.rest.webservices.restfulwebservices.todo;
+
+import java.util.*;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoHardcodedService {
+	private static List<Todo> todos=new ArrayList<>();
+	private static long idCounter=0;
+	/*static {
+		todos.add(new Todo(++idCounter,"in28minutes","Learn to React 4",new Date(),false,"abc","xhd"));
+		todos.add(new Todo(++idCounter,"in28minutes","Learn to Dance 2",new Date(),false,"abc","xhd"));
+		todos.add(new Todo(++idCounter,"in28minutes","Learn to Service 2",new Date(),false,"abc","xhd"));
+	}*/
+	public List<Todo> findAll(){
+		return todos;
+	}
+	public Todo save(Todo todo) {
+		if(todo.getId()==-1 || todo.getId()==0) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		}else {
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		return todo;
+	}
+	public Todo deleteById(long id) {
+		Todo todo=findById(id);
+		if(todo ==null) return null;
+		if(todos.remove(todo)) {
+		return todo;
+		}
+		return null;
+	}
+    public Todo findById(long id) {
+		for(Todo todo: todos) {
+			if(todo.getId() == id) {
+				return todo;
+			}
+		}
+			
+			
+		return null;
+	}
+}
